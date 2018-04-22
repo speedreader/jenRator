0dfb05a05c20507f2a89049581c53d52cf390268
speedreader <speedreader@gmail.com>
2018-04-21 19:40:12 -0500

    Update reminder.R

--- R/reminder.R
+++ R/reminder.R
@@ -7,7 +7,7 @@
 #' @examples
 #' reminder("Collette")
 
-reminder <- function(name = 'Jennifer'){ data.frame(t=seq(0, 2*pi, by=0.1) )
+reminder <- function(name = 'Jennifer', font = 'Helvetica'){ data.frame(t=seq(0, 2*pi, by=0.1) )
  xhrt <- function(t) 16*sin(t)^3
  yhrt <- function(t) 13*cos(t)-5*cos(2*t)-2*cos(3*t)-cos(4*t)
  dat$y=yhrt(dat$t)
@@ -18,7 +18,7 @@ textMessage <- c(paste0(name, '\n is \n Awesome'))
 
 ggplot(dat, aes(x,y)) +
   geom_polygon(fill = "hotpink", alpha = 0.9) +
-  annotate("text", x=0, y= - 1, label = textMessage, color = 'white', family = 'Mistral', lineheight = .75, size = 18, angle = 45) +
+  annotate("text", x=0, y= - 1, label = textMessage, color = 'white', family = font, lineheight = .75, size = 18, angle = 45) +
    theme_void() 
 
 }

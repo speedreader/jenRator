74e9924e7c9852daa67e077a20f148d7ce77b299
speedreader <speedreader@gmail.com>
2018-04-23 18:19:39 -0500

    Update reminder.R

--- R/reminder.R
+++ R/reminder.R
@@ -7,7 +7,8 @@
 #' @examples
 #' reminder("Collette")
 
-reminder <- function(name = 'Jennifer', font = 'Helvetica'){ data.frame(t=seq(0, 2*pi, by=0.1) )
+reminder <- function(name = 'Jennifer', font = 'Helvetica'){ 
+ dat <- data.frame(t=seq(0, 2*pi, by=0.1) )
  xhrt <- function(t) 16*sin(t)^3
  yhrt <- function(t) 13*cos(t)-5*cos(2*t)-2*cos(3*t)-cos(4*t)
  dat$y=yhrt(dat$t)

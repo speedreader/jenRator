feae8cf67309f994741da1769f1f98358c450065
speedreader <speedreader@gmail.com>
2018-04-28 11:56:16 -0500

    Update reminder.R

--- R/reminder.R
+++ R/reminder.R
@@ -1,26 +1,28 @@
 #' A Heartfelt Reminder
 #'
 #' This function allows you to remind yourself or someone else that they are awesome.
-#' @param name Defaults to "Jennifer" (probably because she's awesome).
+#' @param name Defaults to "Jennifer" (probably because she's awesome)
+#' @param font Defaults to "Helvetica"
 #' @keywords maybe miguel
 #' @export
+#' @import ggplot2
 #' @examples
-#' reminder("Collette")
+#' reminder(name = "Lisa", font = 'Mistral')
 
 reminder <- function(name = 'Jennifer', font = 'Helvetica'){ 
- dat <- data.frame(t=seq(0, 2*pi, by=0.1) )
- xhrt <- function(t) 16*sin(t)^3
- yhrt <- function(t) 13*cos(t)-5*cos(2*t)-2*cos(3*t)-cos(4*t)
- dat$y=yhrt(dat$t)
- dat$x=xhrt(dat$t)
- #with(dat, plot(x,y, type="l"))
+
+ corazon <- data.frame(t=seq(0, 2*pi, by=0.1) )
+ xHeart <- function(t) 16*sin(t)^3
+ yHeart <- function(t) 13*cos(t)-5*cos(2*t)-2*cos(3*t)-cos(4*t)
+ corazon$y=yHeart(corazon$t)
+ corazon$x=xHeart(corazon$t)
+
 
 textMessage <- c(paste0(name, '\n is \n Awesome'))
 
-ggplot(dat, aes(x,y)) +
+ggplot(corazon, aes(x,y)) +
   geom_polygon(fill = "hotpink", alpha = 0.9) +
   annotate("text", x=0, y= - 1, label = textMessage, color = 'white', family = font, lineheight = .75, size = 18, angle = 45) +
    theme_void() 
 
 }
-
